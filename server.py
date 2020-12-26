73cac87fd70f328fdc942c57320bb47eb9b9c4eb
saeede Masoudinejad <saide.masoudi@gmail.com>
2020-12-27 02:12:25 +0330

    get the json command

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 import zmq
 
 # ZeroMQ Context
@@ -9,6 +12,11 @@ sock.bind("tcp://127.0.0.1:5678")
 
 # Run a simple "Echo" server
 while True:
-    message = sock.recv_string()
-    sock.send_string(message+"saeede")
-    print(message)
+    message = sock.recv_json()
+    dic_mess = json.loads(message)
+    if dic_mess['command_type'] == 'os':
+        command = '{} {}'.format(dic_mess['command_name'], dic_mess['parameters'][0])
+        print("the command is {}".format(command))
+        result = os.system(command)  # TODo: create the os command convert to a function
+    sock.send_json({'result': result})
+    print(result)
